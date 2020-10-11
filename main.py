5eba2eacc08ada235c8baca23af5867aafea06b4
root <root@ip-172-31-6-76.us-east-2.compute.internal>
2020-10-11 10:49:02 +0000

    Hello

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from app.config import WEBHOOK_PORT, WEBHOOK_LISTEN
 if __name__ == "__main__":
     # python manage.py                      # shows available commands
     # python manage.py runserver --help     # shows available runserver options
-    create_app().run(WEBHOOK_LISTEN, WEBHOOK_PORT, debug=True)
+    create_app().run(WEBHOOK_LISTEN, 443, debug=True)
